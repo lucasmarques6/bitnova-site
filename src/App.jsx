b90804342a94175407284db3916b36ab4464bc41
lucas <lucasmarques6@hotmail.com>
2025-06-16 13:52:28 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Bienvenido a Bitnova</h1>
+      <p>Soluciones de software para PYMEs y sector público.</p>
+    </div>
+  );
+}
+export default App;
